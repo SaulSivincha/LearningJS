b78391443ef13c0ae0a3b13957ca479296d40c0c
SaulSivincha <ssivincham@unsa.edu.pe>
2025-01-19 21:52:00 -0500

    programacion asincronica

---
+++ Ejercicio8/script.js
@@ -0,0 +1,25 @@
+let pedido = prompt("Ingrese su pedido")
+
+function obtenerPedido() {
+	return new Promise((resolve, reject) => {
+  setTimeout(() => {
+   const estadoPedido = false;
+			if (estadoPedido) {
+    resolve(`Pedido: ${pedido}.`);
+			} else {
+    reject("Error al obtener el pedido");
+			}
+		}, 5000);
+	});
+}
+
+async function procesarPedido() {
+ try {
+  const pedido = await obtenerPedido();
+		console.log(pedido);
+	} catch (error) {
+  console.error(error);
+	}
+}
+
+procesarPedido();
