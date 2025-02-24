7b5f113f539de76e2353588c96c98612039b179e
SaulSivincha <ssivincham@unsa.edu.pe>
2025-02-24 15:52:04 -0500

    ejercicio con constatntes y variables

---
+++ HackerRankJS/PI/script.js
@@ -0,0 +1,11 @@
+function main(){
+ const PI = Math.PI;
+
+	let r = parseFloat(readLine());
+
+	let area = PI * r ** 2;
+	let perimeter = PI * r * 2;
+
+	console.log(area);
+	console.log(perimeter);
+}
